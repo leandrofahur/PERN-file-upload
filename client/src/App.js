1d92207315b479b484d32a155ffb8cd94e687506
Leandro <fahurleandro@gmail.com>
2021-05-31 21:32:59 -0700

    upload and diplay image

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,13 @@ function App() {
   const onSubmitForm = async (e) => {
     e.preventDefault();
 
+    console.log(image);
     const formData = new FormData();
     formData.append("image", image);
 
     const response = await axios.post(
       "http://localhost:5000/upload",
-      { formData },
+      formData,
       {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -24,19 +25,42 @@ function App() {
   };
 
   return (
-    <div>
-      <form encType="multipart/form-data" onSubmit={(e) => onSubmitForm(e)}>
-        <input
-          type="file"
-          name="image"
-          onChange={(e) => setImage(e.target.files[0])}
-        />
-        <div>
-          <button style={{ marginTop: "20px" }} type="submit">
-            Submit
-          </button>
+    <div className="ui container" style={{ paddingTop: "25%" }}>
+      <div className="ui centered card">
+        <div
+          className="ui image centered"
+          // style={{ width: "100%", margin: "50%" }}
+        >
+          <img
+            src={`${URL.createObjectURL(image)}`}
+            height="300px"
+            width="300px"
+          />
+          {/* <i class="user circle icon"></i> */}
         </div>
-      </form>
+        <div className="content">
+          <form
+            encType="multipart/form-data"
+            className="ui form"
+            onSubmit={(e) => onSubmitForm(e)}
+          >
+            <input
+              type="file"
+              name="image"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
+            <div>
+              <button
+                style={{ marginTop: "20px" }}
+                className="ui primary button fluid"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,17 +3,26 @@ const app = express();
 const cors = require("cors");
 const multer = require("multer");
 
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "data/images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + "-" + file.originalname);
+  },
+});
+
 app.use(express.json());
 app.use(cors());
-app.use(multer({ dest: "data" }).single("image"));
+app.use(multer({ storage }).single("image"));
 
 app.post("/upload", async (req, res) => {
   try {
     res.status(200).json({ msg: "Ok" });
   } catch (error) {
     console.error(error.messge);
-    res.status(400).json({ msg: "Nok" });
+    res.status(400).json({ msg: "Error" });
   }
 });
 
-app.listen("5000", () => console.log(`Servr up and running on port 5000`));
+app.listen("5000", () => console.log(`Server up and running on port 5000`));
