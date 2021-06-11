06c1696d05aefca6463a168d7a518896a0148480
Leandro <fahurleandro@gmail.com>
2021-06-11 16:13:23 -0700

    update

--- server/index.js
+++ server/index.js
@@ -17,6 +17,16 @@ app.use(cors());
 app.use(multer({ storage }).single("image"));
 
 app.post("/upload", async (req, res) => {
+  const image = req.file;
+  if (image) {
+    return res.status(402).json({
+      error: "Attached file is not an image",
+    });
+  }
+
+  const imageUrl = image.path;
+  // send to the database the imageUrl.
+
   try {
     res.status(200).json({ msg: "Ok" });
   } catch (error) {
